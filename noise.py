c90fc7d165865fe5a0461fec854d3571cfc2ba51
AidenMains7 <122924102+AidenMains7@users.noreply.github.com>
2025-04-13 11:43:02 -0400

    Update noise.py

--- noise.py
+++ noise.py
@@ -3,13 +3,15 @@ import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_fock_noise_comparison():
+
+def add_gaussian_noise(wigner, sigma:float):
+  return wigner + np.random.normal(0, sigma**2, size=wigner.shape)
+
+def plot_fock_noise_comparison(sigma):
   fock = dq.fock(8, 3)
   x, y, z = dq.wigner(fock)
-  sigma = 0.125
-  gaussian_noise = np.random.normal(0, sigma, size=z.shape)
+  noisy_wigner = add_gaussian_noise(z, np.sqrt(0.125))
   
-  noisy_wigner = z + gaussian_noise
   fig, axs = plt.subplots(1, 2)
   im0 = axs[0].imshow(z, cmap="RdBu")
   im1 = axs[1].imshow(noisy_wigner, cmap="RdBu")
@@ -22,8 +24,6 @@ def plot_fock_noise_comparison():
       spine.set_linewidth(1)
     ax.tick_params(axis="both", color="black", labelcolor="black",bottom=False,top=False,left=False,right=False,
                    labelbottom=False,labelleft=False)
-  
-  
   plt.tight_layout()
   plt.colorbar(im1, ax=axs, orientation="horizontal")
   fig.subplots_adjust(bottom=0.2)
